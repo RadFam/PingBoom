4f15dbd708012d17d7ca80c4a5abde25d429885d
RadFam <artkorkh@gmail.com>
2021-02-19 18:12:49 +0300

    Initial commit

---
+++ PingBoom/Assets/Scripts/CommonClickControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameObjects;
+
+namespace Controls
+{
+    public class CommonClickControl : MonoBehaviour
+    {
+		Camera cam;
+		PlayerMoveControl playerMoveControl;
+
+		Vector2 mouseClick;
+		Vector2 playerClick;
+
+		Vector2 directionVect;
+		float forceValue;
+
+		[SerializeField]
+		float strengthForce;
+        // Use this for initialization
+        void Awake()
+        {
+			playerMoveControl = GameObject.Find("Player").GetComponent<PlayerMoveControl>();
+			cam = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+			if (Input.GetMouseButtonUp(0))
+			{
+				// Get coordinates of mouse
+				mouseClick = cam.ScreenToWorldPoint(Input.mousePosition);
+				playerClick = playerMoveControl.transform.position;
+
+				directionVect = playerClick - mouseClick;
+				forceValue = directionVect.magnitude * strengthForce;
+				directionVect.Normalize();
+
+				playerMoveControl.StrikePuck(directionVect, forceValue);
+			}
+        }
+    }
+}
---
+++ PingBoom/Assets/Scripts/PlayerMoveControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameObjects
+{
+    public class PlayerMoveControl : MonoBehaviour
+    {
+		Rigidbody2D myRigid;
+		bool isSliding;
+		[SerializeField]
+		float viscousFriction;
+		float minVelocity;
+		float timerCheck = 0.1f;
+		float timerCnt;
+        // Use this for initialization
+        void Awake()
+        {
+			myRigid = gameObject.GetComponent<Rigidbody2D>();
+			isSliding = false;
+			timerCnt = 0.0f;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+			if (isSliding)
+			{
+				timerCnt += Time.deltaTime;
+				if (timerCnt >= timerCheck)
+				{
+					if (myRigid.velocity.magnitude <= minVelocity)
+					{
+						myRigid.velocity = new Vector2(0.0f, 0.0f);
+						isSliding = false;
+					}
+					timerCnt = 0.0f;
+				}
+
+				// Add viscous friction
+				myRigid.AddForce(new Vector2(myRigid.velocity.x * (-1.0f) * viscousFriction, myRigid.velocity.y * (-1.0f) * viscousFriction));
+			}
+        }
+
+		public void StrikePuck(Vector2 direction, float force)
+		{
+			if (!isSliding)
+			{
+				myRigid.AddForce(direction * force, ForceMode2D.Impulse);
+				timerCnt = 0.0f;
+				isSliding = true;
+			}
+		}
+    }
+}
