cf8bfe6258965d843b298cff9926a07cfd6c4b71
RadFam <artkorkh@gmail.com>
2021-03-01 18:50:28 +0300

    Show force and direction of the puck

---
+++ PingBoom/Assets/Scripts/ForceTailScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameObjects
+{
+    public class ForceTailScript : MonoBehaviour
+    {
+		SpriteRenderer mySpriteRender;
+		Color colorMin;
+		Color colorMax;
+
+		[SerializeField]
+		float forceMin;
+		[SerializeField]
+		float forceMax;
+        // Use this for initialization
+        void Awake()
+        {
+			colorMin = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+			colorMax = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+			mySpriteRender = gameObject.GetComponent<SpriteRenderer>();
+        }
+
+        public void OnEnable()
+		{
+			transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+			transform.localScale = new Vector3(2.0f, 1.0f, 1.0f);
+			mySpriteRender.color = colorMin;
+		}
+
+		public void OnDisable()
+		{
+			transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+			transform.localScale = new Vector3(2.0f, 1.0f, 1.0f);
+			mySpriteRender.color = colorMin;
+		}
+
+		public void SetForceAngle(float force, float angle)
+		{
+			float currForce = Mathf.Min(force, forceMax);
+			currForce = Mathf.Max(currForce, forceMin);
+
+			transform.eulerAngles = new Vector3(0.0f, 0.0f, angle-90.0f);
+			transform.localScale = new Vector3(2.0f, 1.0f+currForce, 1.0f);
+			mySpriteRender.color = Color.Lerp(colorMin, colorMax, (currForce - forceMin)/(forceMax - forceMin));
+		}
+    }
+}
---
+++ PingBoom/Assets/Scripts/PlayerTapControl.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using GameObjects;
+
+namespace Controls
+{
+    public class PlayerTapControl : MonoBehaviour, IPointerDownHandler
+    {
+		Camera cam;
+		PlayerMoveControl myPlayerMove;
+		ForceTailScript myForceTail;
+
+		Vector2 directionVect;
+		float forceValue;
+		float forceExpand;
+
+		[SerializeField]
+		float strengthForce;
+
+		bool isTapped;
+        
+        void Start()
+        {	
+			myPlayerMove = gameObject.GetComponent<PlayerMoveControl>();
+			myForceTail = transform.GetChild(0).gameObject.GetComponent<ForceTailScript>();
+			cam = Camera.main;
+			isTapped = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+			/*
+			if (Input.GetMouseButtonDown(0))
+			{
+				PrepareForShoot();
+			}
+			*/
+			if (Input.GetMouseButton(0))
+			{
+				AdjustingShoot();
+			}
+			if (Input.GetMouseButtonUp(0))
+			{
+				MakeShoot();
+			}
+        }
+
+		public void OnPointerDown(PointerEventData pointerEventData)
+		{
+			isTapped = true;
+			PrepareForShoot();
+		}
+		void PrepareForShoot()
+		{
+			// Check if we click on object
+			// !!!!
+
+			myForceTail.gameObject.SetActive(true);
+		}
+
+		void AdjustingShoot()
+		{
+			directionVect = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+			forceExpand = directionVect.magnitude * 4;
+			forceValue = directionVect.magnitude * strengthForce;
+			directionVect.Normalize();
+
+			myForceTail.SetForceAngle(forceExpand, Vector3.SignedAngle(Vector3.left, directionVect, Vector3.forward));
+		}
+
+		void MakeShoot()
+		{
+			myForceTail.gameObject.SetActive(false);
+			myPlayerMove.StrikePuck(directionVect, forceValue);
+		}
+    }
+}
